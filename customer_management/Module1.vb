3393b4c713e8662be13cc1f1bcf2f7d2e8c578d9
TuanNK <nktuan1209@gmail.com>
2025-04-10 16:15:46 +0700

    refactor

--- customer_management/Module1.vb
+++ customer_management/Module1.vb
@@ -4,12 +4,10 @@ Module Module1
     Public dml As OdbcCommand
     Public dr As OdbcDataReader
     Public connection As OdbcConnection
-
     Public Sub connect_db()
         connection = New OdbcConnection("DSN=db_customer;")
         If connection.State = ConnectionState.Closed Then
             connection.Open()
-
         End If
     End Sub
 End Module
